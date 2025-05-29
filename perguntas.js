f612200fe1e652cbd999c1bb1b333dcf80560cf0
thaismacedo2 <thais.macedo.goncalves@escola.pr.gov.br>
2025-05-29 12:29:29 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'O que são as fontes renováveis?',
+    'são recursos naturais que se renovam constantemente, como o sol, o vento, a água, a biomassa e o calor geotérmico. Elas são consideradas inesgotáveis e sustentáveis, pois sua utilização não resulta no esgotamento dos recursos. '
+)
+
+criaCartao(
+    'Sociologia',
+    'O que é mais valia?',
+    'efere-se ao valor excedente que o trabalhador gera além do seu salário, e que é apropriado pelo capitalista como lucro'
+)
+
+criaCartao(
+    'Geografia',
+    'O que são as fontes não renováveis?',
+    'ão recursos naturais que, uma vez consumidos, não são rapidamente reabastecidos pela natureza, ou seja, não se renovam em um período de tempo considerado curto em termos humanos. '
+)
+
+criaCartao(
+    'Biotecnologia',
+    'O que é a biotecnologia vermelha?',
+    'é o ramo da biotecnologia focado na saúde humana, medicina e diagnóstico.'
+)
